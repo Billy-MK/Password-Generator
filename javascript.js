0180cc2db3081b9390b19656cee35d60b5c98e12
Billy Keating <wmontoothkeating@gmail.com>
2021-01-12 14:15:00 -0800

    Created HTML with button linking to javascript, removed form

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function generatePassword() {
+    var lowerCase = confirm("Include lower case?");
+    var upperCase = confirm("Include upper case?");
+    var numbers = confirm("Include numbers?");
+    var specialCharacters = confirm("Include special characters?");
+}
+
