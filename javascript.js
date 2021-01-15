3be6363920de1a382057a64e552cc6608babca42
Billy-MK <74787394+Billy-MK@users.noreply.github.com>
2021-01-14 16:41:30 -0800

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function generatePassword() {
-    var lowerCase = confirm("Include lower case?");
-    var upperCase = confirm("Include upper case?");
-    var numbers = confirm("Include numbers?");
-    var specialCharacters = confirm("Include special characters?");
-}
-
